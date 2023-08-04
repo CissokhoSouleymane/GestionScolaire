22456301dc6723d3c7b2f72703fec5430e9d083c
Souleymane <Cissokhosouleymane63@gmail.com>
2023-08-04 03:27:24 +0000

    some changes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CoursController;
 use App\Http\Controllers\EleveController;
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\EnseignantController;
+
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -84,3 +87,68 @@ use Illuminate\Support\Facades\Route;
         Route::get('/enseignant_ins', function () {
             return view('enseignants.inscription');
         });
+Route::get('/', function () {
+    return view('login');
+})->name('login');
+
+
+Route::get('/accueil', function () {
+    return view('welcome');
+    })->name('accueil');
+
+
+
+//Création d'en new route pour l'inscription
+Route::get('/enseignant_ins', function () {
+    return view('enseignants.inscription');
+});
+
+
+//Matiere
+
+Route::get('/matiere_ins', function () {
+    return view('matieres.inscription');
+});
+Route::get('/matieres',[\App\Http\Controllers\MatiereController::class,'index'])->name('matiere.index');
+Route::post('/matiere',[\App\Http\Controllers\MatiereController::class,'store'])->name('matiere.store');
+Route::get('/matiere/{id}/edit',[\App\Http\Controllers\MatiereController::class, 'edit'])->name('matiere.edit');
+Route::get('/matiere/{id}/update',[\App\Http\Controllers\MatiereController::class,'update'])->name('matiere.update');
+Route::put('/matiere/{id}/update',[\App\Http\Controllers\MatiereController::class,'update'])->name('matiere.update');
+Route::get('matiere/{id}/delete',[\App\Http\Controllers\MatiereController::class, 'delete'])->name('matiere.delete');
+
+
+
+Route::get('/classe_ins', function () {
+    return view('classes.inscription');
+});
+Route::get('/classes',[\App\Http\Controllers\ClasseController::class,'index'])->name('classe.index');
+Route::post('/classe',[\App\Http\Controllers\ClasseController::class,'store'])->name('classe.store');
+Route::get('/classe/{id}/edit',[\App\Http\Controllers\ClasseController::class, 'edit'])->name('classe.edit');
+Route::get('/classe/{id}/update',[\App\Http\Controllers\ClasseController::class,'update'])->name('classe.update');
+Route::put('/classe/{id}/update',[\App\Http\Controllers\ClasseController::class,'update'])->name('classe.update');
+Route::get('classe/{id}/delete',[\App\Http\Controllers\ClasseController::class, 'delete'])->name('classe.delete');
+
+
+
+
+Route::get('/inscription_ins', function () {
+    return view('inscriptions.inscription');
+});
+Route::get('/inscriptions',[\App\Http\Controllers\InscriptionController::class,'index'])->name('inscription.index');
+Route::post('/inscription',[\App\Http\Controllers\InscriptionController::class,'store'])->name('inscription.store');
+Route::get('/inscription/{id}/edit',[\App\Http\Controllers\InscriptionController::class, 'edit'])->name('inscription.edit');
+Route::get('/inscription/{id}/update',[\App\Http\Controllers\InscriptionController::class,'update'])->name('inscription.update');
+Route::put('/inscription/{id}/update',[\App\Http\Controllers\InscriptionController::class,'update'])->name('inscription.update');
+Route::get('inscription/{id}/delete',[\App\Http\Controllers\InscriptionController::class, 'delete'])->name('inscription.delete');
+
+
+
+Route::get('/user_ins', function () {
+    return view('users.inscription');
+});
+Route::get('/users',[\App\Http\Controllers\UserController::class,'index'])->name('user.index');
+Route::post('/user',[\App\Http\Controllers\UserController::class,'store'])->name('user.store');
+Route::get('/user/{id}/edit',[\App\Http\Controllers\UserController::class, 'edit'])->name('user.edit');
+Route::get('/user/{id}/update',[\App\Http\Controllers\UserController::class,'update'])->name('user.update');
+Route::put('/user/{id}/update',[\App\Http\Controllers\UserController::class,'update'])->name('user.update');
+Route::get('user/{id}/delete',[\App\Http\Controllers\UserController::class, 'delete'])->name('user.delete');
